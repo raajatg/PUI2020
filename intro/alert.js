f43c7c2cd67087e0f530de43a32d2fa98f0634bb
raajatg <raajatg@andrew.cmu.edu>
2020-09-24 11:39:30 -0400

    Update alert.js

--- intro/alert.js
+++ intro/alert.js
@@ -1,7 +1,5 @@
 document.body.onload = alert("Hello World");
 
-document.getElementById("red").onclick = hideProfs();
-
 function hideProfs() {
     var x = document.getElementsByClassName("instructor-name");
     for(let counter=0; counter < x.length; counter++){
